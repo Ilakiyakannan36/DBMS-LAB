080011bd33eedcf9b1185abd06487cf981edee2e
ILAKIYA <ilakiyakannan36@gmail.com>
2026-08-07 10:02:23 +0530

    Update EX2.sql

--- EX2.sql
+++ EX2.sql
@@ -1,16 +1,14 @@
 use db;
 Database changed
-***********Table creation************
+-----------Table creation------------
 
 CREATE TABLE Student (StudentID INT, Name VARCHAR(30), Age INT);
 
-
 CREATE TABLE Courses (CourseID INT, CourseName VARCHAR(20));
-
 
 CREATE TABLE Enrollments (EnrollmentID INT, StudentID INT,CourseID INT,Grade VARCHAR(5));
 
-*************Insert values************
+-------------Insert values------------
 
 INSERT INTO Student VALUES (1, 'Alice', 20);
 INSERT INTO Student VALUES (2, 'Bob', 22);
@@ -26,13 +24,14 @@ INSERT INTO Enrollments VALUES (3, 2, 1, 'A-');
 INSERT INTO Enrollments VALUES (4, 3, 3, 'B+');
 INSERT INTO Enrollments VALUES (5, 3, 2, 'A');
 
-********INNER JOIN: Student's enrollments with course details*********
+--------INNER JOIN: Student's enrollments with course details---------
 
-SELECT Student.StudentID, Student.Name, Student.Age,
-    ->        Courses.CourseID, Courses.CourseName, Enrollments.Grade
-    -> FROM Student
-    -> INNER JOIN Enrollments ON Student.StudentID = Enrollments.StudentID
-    -> INNER JOIN Courses ON Enrollments.CourseID = Courses.CourseID;
+SELECT Students.StudentID, Students.Name, Students.Age,
+       Courses.CourseID, Courses.CourseName, Enrollments.Grade
+FROM Students
+INNER JOIN Enrollments ON Students.StudentID = Enrollments.StudentID
+INNER JOIN Courses ON Enrollments.CourseID = Courses.CourseID;
+
 +-----------+---------+------+----------+------------+-------+
 | StudentID | Name    | Age  | CourseID | CourseName | Grade |
 +-----------+---------+------+----------+------------+-------+
@@ -44,11 +43,12 @@ SELECT Student.StudentID, Student.Name, Student.Age,
 +-----------+---------+------+----------+------------+-------+
 5 rows in set (0.00 sec)
 
-*******LEFT JOIN: All students and their enrollments*******
+-------LEFT JOIN: All students and their enrollments(including students with none)-------
 
-SELECT Student.StudentID, Student.Name, Student.Age,Courses.CourseID, Courses.CourseName, Enrollments.Grade
-FROM Student
-LEFT JOIN Enrollments ON Student.StudentID = Enrollments.StudentID
+SELECT Students.StudentID, Students.Name, Students.Age,
+       Courses.CourseID, Courses.CourseName, Enrollments.Grade
+FROM Students
+LEFT JOIN Enrollments ON Students.StudentID = Enrollments.StudentID
 LEFT JOIN Courses ON Enrollments.CourseID = Courses.CourseID;
 +-----------+---------+------+----------+------------+-------+
 | StudentID | Name    | Age  | CourseID | CourseName | Grade |
@@ -61,12 +61,13 @@ LEFT JOIN Courses ON Enrollments.CourseID = Courses.CourseID;
 +-----------+---------+------+----------+------------+-------+
 5 rows in set (0.00 sec)
 
-******RIGHT JOIN: All courses/enrollments (including courses with none)*****
-
-SELECT Student.StudentID, Student.Name, Student.Age,Courses.CourseID, Courses.CourseName, Enrollments.Grade
+-----RIGHT JOIN: All courses/enrollments (including courses with none)-----
+    
+SELECT Students.StudentID, Students.Name, Students.Age,
+       Courses.CourseID, Courses.CourseName, Enrollments.Grade
 FROM Courses
 RIGHT JOIN Enrollments ON Courses.CourseID = Enrollments.CourseID
-RIGHT JOIN Student ON Enrollments.StudentID = Student.StudentID;
+RIGHT JOIN Students ON Enrollments.StudentID = Students.StudentID;
 +-----------+---------+------+----------+------------+-------+
 | StudentID | Name    | Age  | CourseID | CourseName | Grade |
 +-----------+---------+------+----------+------------+-------+
