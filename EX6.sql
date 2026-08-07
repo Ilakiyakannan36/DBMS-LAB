9f43af1853eeaf2f2f1ca9608f96dbc18c9f26f2
ILAKIYA <ilakiyakannan36@gmail.com>
2026-08-07 10:34:22 +0530

    Update EX6.sql

--- EX6.sql
+++ EX6.sql
@@ -1,32 +1,33 @@
-mysql> DELIMITER ;
-mysql> CREATE TABLE customer_update_log (
-    ->     sid INT,
-    ->     old_total INT,
-    ->     new_total INT,
-    ->     action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    -> );
-Query OK, 0 rows affected (0.10 sec)
+-----------TRIGGER ON UPDATE------------
 
-mysql> DELIMITER //
-mysql> 
-mysql> CREATE TRIGGER up_classd
-    -> BEFORE UPDATE ON customer
-    -> FOR EACH ROW
-    -> BEGIN
-    ->     INSERT INTO customer_update_log(sid, old_total, new_total)
-    ->     VALUES(OLD.sid, OLD.stotal, NEW.stotal);
-    -> END//
-Query OK, 0 rows affected (0.03 sec)
+CREATE TABLE customer_update_log (
+    sid INT,
+    old_total INT,
+    new_total INT,
+    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
-mysql> 
-mysql> DELIMITER ;
-mysql> UPDATE customer
+DELIMITER //
+
+CREATE TRIGGER up_classd
+BEFORE UPDATE ON customer
+FOR EACH ROW
+BEGIN
+    INSERT INTO customer_update_log(sid, old_total, new_total)
+    VALUES (OLD.sid, OLD.stotal, NEW.stotal);
+END//
+
+DELIMITER ;
+
+----------EXECUTION & OUTPUT------------------
+
+UPDATE customer
     -> SET stotal = 1000
     -> WHERE sid = 3;
-Query OK, 1 row affected (0.03 sec)
+
 Rows matched: 1  Changed: 1  Warnings: 0
 
-mysql> SELECT * FROM customer_update_log;
+SELECT * FROM customer_update_log;
 +------+-----------+-----------+---------------------+
 | sid  | old_total | new_total | action_time         |
 +------+-----------+-----------+---------------------+
@@ -34,18 +35,22 @@ mysql> SELECT * FROM customer_update_log;
 +------+-----------+-----------+---------------------+
 1 row in set (0.00 sec)
 
-mysql> CREATE TABLE customer_delete_log (
+
+
+-----------TRIGGER ON DELETE------------
+
+CREATE TABLE customer_delete_log (
     ->     sid INT,
     ->     sname VARCHAR(50),
     ->     stotal INT,
     ->     action VARCHAR(20),
     ->     action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     -> );
-Query OK, 0 rows affected (0.14 sec)
 
-mysql> DELIMITER //
-mysql> 
-mysql> CREATE TRIGGER del_classb
+
+DELIMITER //
+
+CREATE TRIGGER del_classb
     -> BEFORE DELETE ON customer
     -> FOR EACH ROW
     -> BEGIN
@@ -58,15 +63,18 @@ mysql> CREATE TRIGGER del_classb
     ->         NOW()
     ->     );
     -> END//
-Query OK, 0 rows affected (0.02 sec)
 
-mysql> 
-mysql> DELIMITER ;
-mysql> DELETE FROM customer
+
+
+DELIMITER ;
+
+----------EXECUTION & OUTPUT------------------
+
+DELETE FROM customer
     -> WHERE sid = 1;
-Query OK, 1 row affected (0.02 sec)
 
-mysql> SELECT * FROM customer_delete_log;
+
+SELECT * FROM customer_delete_log;
 +------+-------+--------+---------+---------------------+
 | sid  | sname | stotal | action  | action_time         |
 +------+-------+--------+---------+---------------------+
@@ -74,18 +82,20 @@ mysql> SELECT * FROM customer_delete_log;
 +------+-------+--------+---------+---------------------+
 1 row in set (0.00 sec)
 
-mysql> CREATE TABLE classb (
+-----------TRIGGER ON INSERT (Validation)------------
+
+CREATE TABLE classb (
     ->     sid INT PRIMARY KEY,
     ->     sname VARCHAR(50),
     ->     sdept VARCHAR(20),
     ->     stotal INT,
     ->     grade CHAR(1)
     -> );
-Query OK, 0 rows affected (0.10 sec)
 
-mysql> DELIMITER //
-mysql> 
-mysql> CREATE TRIGGER ins_classb
+
+DELIMITER //
+
+CREATE TRIGGER ins_classb
     -> BEFORE INSERT ON classb
     -> FOR EACH ROW
     -> BEGIN
@@ -94,15 +104,18 @@ mysql> CREATE TRIGGER ins_classb
     ->         SET MESSAGE_TEXT = 'Total not valid';
     ->     END IF;
     -> END//
-Query OK, 0 rows affected (0.02 sec)
 
-mysql> 
-mysql> DELIMITER ;
-mysql> INSERT INTO classb VALUES
+
+
+DELIMITER ;
+
+------------VALID AND INVALID INSERT & TESTING----------------
+
+INSERT INTO classb VALUES
     -> (1, 'John', 'IT', 900, 'A');
-Query OK, 1 row affected (0.02 sec)
 
-mysql> SELECT * FROM classb;
+
+SELECT * FROM classb;
 +-----+-------+-------+--------+-------+
 | sid | sname | sdept | stotal | grade |
 +-----+-------+-------+--------+-------+
@@ -110,7 +123,7 @@ mysql> SELECT * FROM classb;
 +-----+-------+-------+--------+-------+
 1 row in set (0.00 sec)
 
-mysql> INSERT INTO classb VALUES
+INSERT INTO classb VALUES
     -> (2, 'Jana', 'IT', 20000, 'A');
 ERROR 1644 (45000): Total not valid
 
