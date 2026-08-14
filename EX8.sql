baac4ecb1bb593f736d3b58fda34574f504bfa81
ILAKIYA <ilakiyakannan36@gmail.com>
2026-08-14 11:59:47 +0530

    Update EX8.sql

--- EX8.sql
+++ EX8.sql
@@ -1,9 +1,10 @@
-use db;
-Database changed
+USE db;
 
--------------Retrieve the name and address of a customer and handle exceptions------------
+-- Retrieve the name and address of a customer and handle exceptions
 
-select* from customers;
+SELECT * FROM customers;
+Expected Output
+Customers Table
 +----+-------+-------------+---------+
 | id | name  | address     | salary  |
 +----+-------+-------------+---------+
@@ -13,57 +14,51 @@ select* from customers;
 |  4 | David | Houston     | 7500.00 |
 |  5 | Emma  | Boston      | 6000.00 |
 +----+-------+-------------+---------+
-5 rows in set (0.00 sec)
 
-
-------------------PREDEFINED EXCEPTION HANDLING--------------------
----------------Procedure 1: GetCustomer()------------------
+-- PREDEFINED EXCEPTION HANDLING
+-- Procedure 1: GetCustomer()
 
 DELIMITER //
 
 CREATE PROCEDURE GetCustomer()
-    -> BEGIN
-    ->     DECLARE c_id INT DEFAULT 5;
-    ->     DECLARE c_name VARCHAR(100);
-    ->     DECLARE c_addr VARCHAR(255);
-    -> 
-    ->     DECLARE CONTINUE HANDLER FOR NOT FOUND
-    ->     BEGIN
-    ->         SELECT 'No such customer!' AS Message;
-    ->     END;
-    -> 
-    ->     DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    ->     BEGIN
-    ->         SELECT 'Error!' AS Message;
-    ->     END;
-    -> 
-    ->     SELECT name, address 
-    ->     INTO c_name, c_addr
-    ->     FROM customer
-    ->     WHERE id = c_id;
-    -> 
-    ->     SELECT CONCAT('Name: ', c_name) AS Output;
-    ->     SELECT CONCAT('Address: ', c_addr) AS Output;
-    -> 
-    -> END //
+BEGIN
+    DECLARE c_id INT DEFAULT 5;
+    DECLARE c_name VARCHAR(100);
+    DECLARE c_addr VARCHAR(255);
 
--------------------Calling PROCEDURE 1---------------------
+    DECLARE CONTINUE HANDLER FOR NOT FOUND
+    BEGIN
+        SELECT 'No such customer!' AS Message;
+    END;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SELECT 'Error!' AS Message;
+    END;
+
+    SELECT name, address
+    INTO c_name, c_addr
+    FROM customer
+    WHERE id = c_id;
+
+    SELECT CONCAT('Name: ', c_name) AS Output;
+    SELECT CONCAT('Address: ', c_addr) AS Output;
+END//
 
 DELIMITER ;
 
+-- Call Procedure 1
+
 CALL GetCustomer();
+Expected Output
 +---------+
 | Message |
 +---------+
 | Error!  |
 +---------+
-1 row in set (0.00 sec)
 
-DELIMITER //
-
-
-------------------USER-DEFINED EXCEPTION HANDLING--------------------
----------------Procedure 2: CheckSalary()------------------
+-- USER-DEFINED EXCEPTION HANDLING
+-- Procedure 2: CheckSalary()
 
 DELIMITER //
 
@@ -77,61 +72,61 @@ BEGIN
     ELSE
         SELECT 'Valid Salary' AS Message;
     END IF;
-END //
+END//
 
 DELIMITER ;
 
--------------------Calling PROCEDURE 2---------------------
+-- Call Procedure 2
 
 CALL CheckSalary(5000);
-
+Expected Output
 +--------------+
 | Message      |
 +--------------+
 | Valid Salary |
 +--------------+
 
-------------------BOTH PREDEFINED EXCEPTION & USER-DEFINED EXCEPTION HANDLING--------------------
----------------Procedure 3: CheckCustomer()------------------
+-- BOTH PREDEFINED AND USER-DEFINED EXCEPTION HANDLING
+-- Procedure 3: CheckCustomer()
+
+DELIMITER //
 
 CREATE PROCEDURE CheckCustomer(IN cc_id INT)
-    -> BEGIN
-    ->     DECLARE c_name VARCHAR(100);
-    ->     DECLARE c_addr VARCHAR(255);
-    -> 
-    ->     DECLARE invalid_id CONDITION FOR SQLSTATE '45000';
-    -> 
-    ->     DECLARE EXIT HANDLER FOR NOT FOUND
-    ->     BEGIN
-    ->         SELECT 'No such customer!' AS Message;
-    ->     END;
-    -> 
-    ->     DECLARE EXIT HANDLER FOR SQLEXCEPTION
-    ->     BEGIN
-    ->         SELECT 'Error!' AS Message;
-    ->     END;
-    -> 
-    ->     IF cc_id <= 0 THEN
-    ->         SIGNAL invalid_id
-    ->         SET MESSAGE_TEXT = 'ID must be greater than zero!';
-    ->     ELSE
-    -> 
-    ->         SELECT name, address
-    ->         INTO c_name, c_addr
-    ->         FROM customers
-    ->         WHERE id = cc_id;
-    -> 
-    ->         SELECT CONCAT('Name: ', c_name) AS Output;
-    ->         SELECT CONCAT('Address: ', c_addr) AS Output;
-    -> 
-    ->     END IF;
-    -> 
-    -> END //
+BEGIN
+    DECLARE c_name VARCHAR(100);
+    DECLARE c_addr VARCHAR(255);
 
+    DECLARE invalid_id CONDITION FOR SQLSTATE '45000';
+
+    DECLARE EXIT HANDLER FOR NOT FOUND
+    BEGIN
+        SELECT 'No such customer!' AS Message;
+    END;
+
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION
+    BEGIN
+        SELECT 'Error!' AS Message;
+    END;
+
+    IF cc_id <= 0 THEN
+        SIGNAL invalid_id
+        SET MESSAGE_TEXT = 'ID must be greater than zero!';
+    ELSE
+
+        SELECT name, address
+        INTO c_name, c_addr
+        FROM customers
+        WHERE id = cc_id;
+
+        SELECT CONCAT('Name: ', c_name) AS Output;
+        SELECT CONCAT('Address: ', c_addr) AS Output;
+
+    END IF;
+END//
 
 DELIMITER ;
 
--------------------Calling PROCEDURE 3---------------------
+-- Call Procedure 3
 
 CALL CheckCustomer(5);
 +------------+
@@ -139,32 +134,13 @@ CALL CheckCustomer(5);
 +------------+
 | Name: Emma |
 +------------+
-1 row in set (0.00 sec)
-
-+-----------------+
-| Output          |
-+-----------------+
-| Address: Boston |
-+-----------------+
-1 row in set (0.00 sec)
-
-
 
 CALL CheckCustomer(5);
-+------------+
-| Output     |
-+------------+
-| Name: Emma |
-+------------+
-1 row in set (0.00 sec)
-
 +-----------------+
 | Output          |
 +-----------------+
 | Address: Boston |
 +-----------------+
-1 row in set (0.00 sec)
-
 
 CALL GetCustomer();
 +---------+
@@ -172,7 +148,3 @@ CALL GetCustomer();
 +---------+
 | Error!  |
 +---------+
-1 row in set (0.01 sec)
-
-
-
