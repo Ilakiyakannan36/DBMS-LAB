9b3ce6760701a74b20d67f16bc9b29e67bc3e75c
ILAKIYA <ilakiyakannan36@gmail.com>
2026-09-19 14:24:46 +0530

    Update EX6.sql

--- EX6.sql
+++ EX6.sql
@@ -1,120 +1,68 @@
------------TRIGGER ON UPDATE------------
-
-CREATE TABLE customer_update_log (
-    sid INT,
-    old_total INT,
-    new_total INT,
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-DELIMITER //
-
-CREATE TRIGGER up_classd
+-- TRIGGER ON UPDATE
+CREATE OR REPLACE TRIGGER up_classd
 BEFORE UPDATE ON customer
 FOR EACH ROW
+DECLARE
 BEGIN
-    INSERT INTO customer_update_log(sid, old_total, new_total)
-    VALUES (OLD.sid, OLD.stotal, NEW.stotal);
-END//
+   IF UPDATING THEN
+      DBMS_OUTPUT.PUT_LINE('new value is ' || :NEW.stotal);
+      DBMS_OUTPUT.PUT_LINE('old value is ' || :OLD.stotal);
+   END IF;
+END;
+/
 
-DELIMITER ;
+UPDATE customer 
+SET salary=500 
+WHERE sid=3;
 
-----------EXECUTION & OUTPUT------------------
+new value is 500
+old value is 900
+1 row updated.
 
-UPDATE customer
-SET stotal = 1000
-WHERE sid = 3;
-
-Rows matched: 1  Changed: 1  Warnings: 0
-
-SELECT * FROM customer_update_log;
-+------+-----------+-----------+---------------------+
-| sid  | old_total | new_total | action_time         |
-+------+-----------+-----------+---------------------+
-|    3 |       900 |      1000 | 2026-07-30 11:32:16 |
-+------+-----------+-----------+---------------------+
-1 row in set (0.00 sec)
-
-
-
------------TRIGGER ON DELETE------------
-
-CREATE TABLE customer_delete_log (
-    sid INT,
-    sname VARCHAR(50),
-    stotal INT,
-    action VARCHAR(20),
-    action_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-DELIMITER //
-
-CREATE TRIGGER del_classb
+-- TRIGGER ON DELETE
+CREATE OR REPLACE TRIGGER del_classb
 BEFORE DELETE ON customer
 FOR EACH ROW
+DECLARE
 BEGIN
-    INSERT INTO customer_delete_log
-    VALUES (
-        OLD.sid,
-        OLD.sname,
-        OLD.stotal,
-        'DELETED',
-        NOW()
-    );
-END//
+   IF DELETING THEN
+      DBMS_OUTPUT.PUT_LINE('row deleted');
+   END IF;
+END;
+/
+    
+DELETE FROM customer WHERE id=1;
 
-DELIMITER ;
-----------EXECUTION & OUTPUT------------------
+row deleted
+1 row deleted.
 
-DELETE FROM customer
-WHERE sid = 1;
-
-
-SELECT * FROM customer_delete_log;
-+------+-------+--------+---------+---------------------+
-| sid  | sname | stotal | action  | action_time         |
-+------+-------+--------+---------+---------------------+
-|    1 | John  |    500 | DELETED | 2026-07-30 11:33:55 |
-+------+-------+--------+---------+---------------------+
-1 row in set (0.00 sec)
-
------------TRIGGER ON INSERT (Validation)------------
-
-CREATE TABLE classb (
-    sid INT PRIMARY KEY,
-    sname VARCHAR(50),
-    sdept VARCHAR(20),
-    stotal INT,
-    grade CHAR(1)
-);
-
-DELIMITER //
-
-CREATE TRIGGER ins_classb
+-- TRIGGER ON INSERT (with validation, table: classb(sid, sname, sdept, stotal, grade))
+CREATE OR REPLACE TRIGGER ins_classb
 BEFORE INSERT ON classb
 FOR EACH ROW
+DECLARE
+   InvTot EXCEPTION;
 BEGIN
-    IF NEW.stotal > 1000 THEN
-        SIGNAL SQLSTATE '45000'
-        SET MESSAGE_TEXT = 'Total not valid';
-    END IF;
-END//
+   IF INSERTING THEN
+      IF :NEW.stotal > 1000 THEN
+         RAISE InvTot;
+      END IF;
+   END IF;
+EXCEPTION
+   WHEN InvTot THEN
+      RAISE_APPLICATION_ERROR(-20000, 'Total not valid');
+END;
+/
 
-DELIMITER ;
-
-------------VALID AND INVALID INSERT & TESTING----------------
-
+-- Test 1: valid insert (stotal <= 1000) -> succeeds
 INSERT INTO classb VALUES (1, 'John', 'IT', 900, 'A');
 
-
 SELECT * FROM classb;
-+-----+-------+-------+--------+-------+
-| sid | sname | sdept | stotal | grade |
-+-----+-------+-------+--------+-------+
-|   1 | John  | IT    |    900 | A     |
-+-----+-------+-------+--------+-------+
-1 row in set (0.00 sec)
 
-INSERT INTO classb VALUES (2, 'Jana', 'IT', 20000, 'A');
-ERROR 1644 (45000): Total not valid
+sid | sname | sdept | stotal | grade
+----+-------+-------+--------+------
+1   | John  | IT    | 900    | A
 
+
+INSERT INTO classb VALUES('6','jana','it','20000','a');
+Invalid insert rejected: ORA-20000: Total not valid
